90a29ddfc11b5036bfdcba9e7d658acca7e5f933
ORION <81926548+OrionDevInfo@users.noreply.github.com>
2022-05-23 21:31:53 +0200

    Fixs

--- utils/database.js
+++ utils/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 module.exports = {
     con: mysql.createConnection({
-        host: "164.132.108.164",
-        user: "orion",
-        password: "mdporion",
-        database: "RiddleProject"
+        host: "",
+        user: "",
+        password: "",
+        database: ""
     })
 };
