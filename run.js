ad440ebd77075b171473fb5e585b6d7b6d0be6e5
ORION <orion.maubert@gmail.com>
2022-05-23 19:09:01 +0200

    Run File

---
+++ run.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs');
+const client = require('./utils/client.js');
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
